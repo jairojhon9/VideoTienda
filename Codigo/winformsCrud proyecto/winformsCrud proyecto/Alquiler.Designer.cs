3c307257a4c6ac8d801de20220e66a3912383e80
haroldjpa <harold_johnnie@outlook.com>
2020-11-28 23:19:38 -0500

    Se agrega el boton en e formulario Alquiler

--- Codigo/winformsCrud proyecto/winformsCrud proyecto/Alquiler.Designer.cs
+++ Codigo/winformsCrud proyecto/winformsCrud proyecto/Alquiler.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+      this.button1 = new System.Windows.Forms.Button();
       this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(312, 190);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(123, 43);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
       // 
       // Alquiler
       // 
@@ -36,12 +46,15 @@
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.button1);
       this.Name = "Alquiler";
       this.Text = "Alquiler";
       this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+  }
 }
