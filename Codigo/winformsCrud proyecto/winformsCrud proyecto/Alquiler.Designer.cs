5edfe9b1312437c764bdae1a07e05ce954bbcc22
haroldjpa <harold_johnnie@outlook.com>
2020-11-28 23:12:20 -0500

    Se cambia el color del formulario alquiler a color verde

--- Codigo/winformsCrud proyecto/winformsCrud proyecto/Alquiler.Designer.cs
+++ Codigo/winformsCrud proyecto/winformsCrud proyecto/Alquiler.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Alquiler";
+      this.SuspendLayout();
+      // 
+      // Alquiler
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Name = "Alquiler";
+      this.Text = "Alquiler";
+      this.ResumeLayout(false);
+
         }
 
         #endregion
